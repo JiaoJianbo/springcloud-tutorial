4095b8c4036cfd9563d329519b3b7b292a631ea9
Bobby Jiao <jiaojb84@gmail.com>
2018-08-02 16:40:20 +0800

    add config server and client

---
+++ cloud-config-client/src/main/java/com/bravo/springcloud/tutorial/config/client/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.bravo.springcloud.tutorial.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中
+@EnableEurekaClient // 将服务注册到Eureka server,将开启自动发现
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+	
+	@Value("${hello}")
+	String hello;
+	
+	@RequestMapping(value = "/hi")
+	public String hello() {
+		return hello;
+	}
+
+}
---
+++ cloud-config-server/src/main/java/com/bravo/springcloud/tutorial/config/server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.bravo.springcloud.tutorial.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+
+@EnableEurekaClient // 将服务注册到Eureka server,将开启自动发现
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
