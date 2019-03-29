24b400f257bd2a40d1ab774cbf57f802ca5ee083
Bobby Jiao <jiaojb84@gmail.com>
2019-03-29 13:19:24 +0800

    Update ignore file.

--- feign-service/src/main/java/com/bravo/springcloud/tutorial/feign/service/client/HelloWorldService.java
+++ feign-service/src/main/java/com/bravo/springcloud/tutorial/feign/service/client/HelloWorldService.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(value = "SERVICE-HELLOWORLD", fallback = HelloWorldServiceFailure.class)
 public interface HelloWorldService {
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/") // 1.不支持 @GetMapping这样的注解。 2.@PathVariable必须显式地设置value，不能使用默认值
 	String sayHello();
 }
